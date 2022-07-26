da3776c19aebd05fb62a0a84a60eb8156112cece
MiguelJDZ <71290776+MiguelJDZ@users.noreply.github.com>
2022-07-26 00:46:41 -0400

    Add files via upload

---
+++ FederalIncomeTaxApp/IncomeTaxCalculatorForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace FederalIncomeTaxApp
+{
+    partial class IncomeTaxCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTaxableIncome = new System.Windows.Forms.Label();
+            this.txtTaxableIncome = new System.Windows.Forms.TextBox();
+            this.lblIncomeTaxOwed = new System.Windows.Forms.Label();
+            this.txtIncomeTaxOwed = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTaxableIncome
+            // 
+            this.lblTaxableIncome.AutoSize = true;
+            this.lblTaxableIncome.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaxableIncome.Location = new System.Drawing.Point(38, 33);
+            this.lblTaxableIncome.Name = "lblTaxableIncome";
+            this.lblTaxableIncome.Size = new System.Drawing.Size(101, 13);
+            this.lblTaxableIncome.TabIndex = 0;
+            this.lblTaxableIncome.Text = "&Taxable Income:";
+            this.lblTaxableIncome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtTaxableIncome
+            // 
+            this.txtTaxableIncome.Location = new System.Drawing.Point(159, 30);
+            this.txtTaxableIncome.Name = "txtTaxableIncome";
+            this.txtTaxableIncome.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxableIncome.TabIndex = 1;
+            // 
+            // lblIncomeTaxOwed
+            // 
+            this.lblIncomeTaxOwed.AutoSize = true;
+            this.lblIncomeTaxOwed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncomeTaxOwed.Location = new System.Drawing.Point(38, 70);
+            this.lblIncomeTaxOwed.Name = "lblIncomeTaxOwed";
+            this.lblIncomeTaxOwed.Size = new System.Drawing.Size(113, 13);
+            this.lblIncomeTaxOwed.TabIndex = 2;
+            this.lblIncomeTaxOwed.Text = "Income Tax Owed:";
+            this.lblIncomeTaxOwed.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtIncomeTaxOwed
+            // 
+            this.txtIncomeTaxOwed.Location = new System.Drawing.Point(159, 67);
+            this.txtIncomeTaxOwed.Name = "txtIncomeTaxOwed";
+            this.txtIncomeTaxOwed.ReadOnly = true;
+            this.txtIncomeTaxOwed.Size = new System.Drawing.Size(100, 20);
+            this.txtIncomeTaxOwed.TabIndex = 3;
+            this.txtIncomeTaxOwed.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(78, 115);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(184, 115);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // IncomeTaxCalculatorForm
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(301, 165);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtIncomeTaxOwed);
+            this.Controls.Add(this.lblIncomeTaxOwed);
+            this.Controls.Add(this.txtTaxableIncome);
+            this.Controls.Add(this.lblTaxableIncome);
+            this.Name = "IncomeTaxCalculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Income Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTaxableIncome;
+        private System.Windows.Forms.TextBox txtTaxableIncome;
+        private System.Windows.Forms.Label lblIncomeTaxOwed;
+        private System.Windows.Forms.TextBox txtIncomeTaxOwed;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ FederalIncomeTaxApp/IncomeTaxCalculatorForm.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * File: IncomeTaxCalculatorForm.cs
+ * Author: Miguel Duran 841-17-1372
+ * Course: COTI 4150-KJ1 Prof. Antonio F. Huertas
+ * Date: 02/18/2021
+ * Purpose: This form calculates and displays the income tax owed for the given
+ *          taxable income and tax percent.
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace FederalIncomeTaxApp
+{
+    public partial class IncomeTaxCalculatorForm : Form
+    {
+        // Creates a new form.
+        public IncomeTaxCalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        // Closes this form when the Exit button is clicked.
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Calculates the income tax owed based on the value entered when the 
+        // Calculate button is clicked.
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal taxIncome = Convert.ToInt32(txtTaxableIncome.Text);
+                decimal taxPercent = 0.0M;
+                decimal taxOwed = FederalTaxIncome(taxIncome, taxPercent);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid number. Value must be an integer.", "Entry Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Overflow error. Enter smaller values.", "Entry Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message, "Processing Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() +
+                    "\n" + ex.StackTrace, "Exception Caught",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        // Calculates the income tax owed based on the given taxable income, tax percent
+        private decimal FederalTaxIncome(decimal taxIncome, decimal taxPercent)
+        {
+            decimal taxOwed = 0.0M;
+            if (taxIncome >= 413200.0M)
+            {
+                taxPercent = .396M;
+                taxOwed = (Decimal.ToInt32((taxIncome - 413200.0M) * taxPercent) + 119996.25M);
+                txtIncomeTaxOwed.Text = taxOwed.ToString("c");
+            }
+            else if (taxIncome >= 411500.0M)
+            {
+                taxPercent = .35M;
+                taxOwed = (Decimal.ToInt32((taxIncome - 411500.0M) * taxPercent) + 119401.25M);
+                txtIncomeTaxOwed.Text = taxOwed.ToString("c");
+            }
+            else if (taxIncome >= 189300.0M)
+            {
+                taxPercent = .33M;
+                taxOwed = (Decimal.ToInt32((taxIncome - 189300.0M) * taxPercent) + 46675.25M);
+                txtIncomeTaxOwed.Text = taxOwed.ToString("c");
+            }
+            else if (taxIncome >= 90750.0M)
+            {
+                taxPercent = .28M;
+                taxOwed = (Decimal.ToInt32((taxIncome - 90750.0M) * taxPercent) + 18481.25M);
+                txtIncomeTaxOwed.Text = taxOwed.ToString("c");
+            }
+            else if (taxIncome >= 37450.0M)
+            {
+                taxPercent = .25M;
+                taxOwed = (Decimal.ToInt32((taxIncome - 37450.0M) * taxPercent) + 5156.25M);
+                txtIncomeTaxOwed.Text = taxOwed.ToString("c");
+            }
+            else if (taxIncome >= 9225.0M)
+            {
+                taxPercent = .15M;
+                taxOwed = (Decimal.ToInt32((taxIncome - 9225.0M) * taxPercent) + 922.50M);
+                txtIncomeTaxOwed.Text = taxOwed.ToString("c");
+            }
+            else if (taxIncome >= 0.0M)
+            {
+                taxPercent = .10M;
+                taxOwed = (Decimal.ToInt32((taxIncome - 0.0M) * taxPercent) + 0M);
+                txtIncomeTaxOwed.Text = taxOwed.ToString("c");
+            }
+            else if (taxIncome < 0.0M)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "Taxable Income", "Taxable Income Must Be Positive");
+            }
+            return taxOwed;
+        }
+
+        // Clears the tax owed textbox when the input text changes.
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            txtIncomeTaxOwed.Clear();
+        }
+    }
+}
